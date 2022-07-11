15b233aed0aedcd10dcf36e7a3478790cfe4bb0b
simon <simon@dancingcloudservices.com>
2022-07-11 09:08:29 -0600

    Initial

---
+++ src/main/java/verify/Verify.java
@@ -0,0 +1,11 @@
+package verify;
+
+public class Verify {
+  public static void main(String[] args) {
+    Object obj = "Hello World";
+    System.out.println(switch(obj){
+      case String s && s.length() > 5 -> s;
+      default -> "oops!";
+    });
+  }
+}
