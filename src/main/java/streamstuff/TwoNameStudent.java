8dcd54d70c867b6cc75690887ef7976bbc3b1f38
simon <simon@dancingcloudservices.com>
2022-07-12 18:02:32 -0600

    Day 2 Final

--- src/main/java/streamstuff/TwoNameStudent.java
+++ src/main/java/streamstuff/TwoNameStudent.java
@@ -4,6 +4,15 @@ import java.util.Comparator;
 import java.util.List;
 
 public class TwoNameStudent {
+  public static <E> Comparator<E> thenComparing(
+      Comparator<E> first, Comparator<E> second) {
+    return (e1, e2) -> {
+      int v1 = first.compare(e1, e2);
+      if (v1 != 0) return v1;
+      else return second.compare(e1, e2);
+    };
+  }
+
   private String first;
   private String last;
 
@@ -39,8 +48,26 @@ public class TwoNameStudent {
         new TwoNameStudent("Felicity", "Jones"),
         new TwoNameStudent("Mary", "Smith")
         );
+    Comparator<TwoNameStudent> ctns = Comparator.comparing(s -> s.getLast());
+//    Comparator<TwoNameStudent> ctnsrev = ctns.reversed();
+//    Comparator<TwoNameStudent> ctnsBothNames =
+//        ctns.thenComparing(s -> s.getFirst());
+
     lts.stream()
-        .sorted(Comparator.comparing(s -> s.getLast()))
-        .forEach(s -> System.out.println(s));
+        .sorted(Comparator
+            .comparing(TwoNameStudent::getLast)
+            .thenComparing(TwoNameStudent::getFirst))
+//        .sorted(Comparator
+//            .<TwoNameStudent, String>comparing(s -> s.getLast())
+//            .thenComparing(s -> s.getFirst()))
+//        .sorted(Comparator
+//            .comparing((TwoNameStudent s) -> s.getLast())
+//            .thenComparing(s -> s.getFirst()))
+//        .sorted(ctns.thenComparing(s -> s.getFirst()))
+//        .forEach(s -> System.out.println(s));
+        .forEach(System.out::println);
+
+
+//    List<String> ls;
   }
 }
